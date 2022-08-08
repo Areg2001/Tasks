e8ea342e2bd1fa4152f90c5546f1c02578a523b5
Areg2001 <107401750+Areg2001@users.noreply.github.com>
2022-08-08 17:15:49 +0400

    Delete homeworks directory

--- homeworks/leetcode1748.py
+++
@@ -1,5 +0,0 @@
-lst = [1, 3, 4, 1, 5, 3, 7, 8]
-def solution(n):
-    return sum([n[i] for i in range(len(n)) if n.count(n[i]) == 1])
-    
-print(solution(lst))
--- homeworks/leetcode58.py
+++
@@ -1,5 +0,0 @@
-s = "   fly me   to   the   moon   "
-def lengthOfLastWord(s):
-    return ((s.split())[-1])
-    
-print(lengthOfLastWord(s))    
