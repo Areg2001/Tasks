03df795fcfcc2cee69868d066af96b071c73c0ac
Areg2001 <107401750+Areg2001@users.noreply.github.com>
2022-08-15 10:57:39 +0400

    Delete task2.py

--- task2.py
+++
@@ -1,9 +0,0 @@
-with open("text.txt", "r") as f:
-    new_code = [row.title() for row in f.readlines()]
-    
-with open('new.txt', 'w') as f:
-    for row in new_code:
-        f.write(row)
-    
-    
-    
